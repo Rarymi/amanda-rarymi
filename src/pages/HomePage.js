7447f71b3a99b456987986203bd56a5757a59bb3
Gabriel Manfroi <gabsmanfroi@gmail.com>
2021-08-01 15:27:44 -0300

    💄Inserted Social Networks

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,27 +1,56 @@
-import classes from './HomePage.module.scss'
-import facebook from '../assets/images/facebook.png'
-import linkedin from '../assets/images/linkedin.png'
-import instagram from '../assets/images/instagram.png'
-import { useState } from 'react'
+import { Link } from '@material-ui/core';
+import { Facebook, GitHub, Instagram, LinkedIn, Phone } from '@material-ui/icons';
+import { useState } from 'react';
+import classes from './HomePage.module.scss';
+
 export default function HomePage() {
-    const [experienciaProfissionalText, setExperienciaProfissionalText] = useState('Experiência Profissional')
+    const [experienciaProfissionalText, setExperienciaProfissionalText] = useState('Experiência Profissional');
+    const [caixaFormacaoText, setCaixaFormacaoText] = useState('Formação');
+    const [caixaCompetenciasText, setcaixaCompetenciasText] = useState('Competências');
+    const [caixaProjetosText, setcaixaProjetosText] = useState('Projetos');
 
     const changeExperienciaProfissionalText = () => {
-        setExperienciaProfissionalText('Pariatur sunt pariatur ut nostrud dolor consectetur et id. Aliqua irure consectetur eu velit fugiat ea quis anim. Velit culpa duis quis qui excepteur excepteur nostrud.')
-    }
+        setExperienciaProfissionalText(
+            'Pariatur sunt pariatur ut nostrud dolor consectetur et id. Aliqua irure consectetur eu velit fugiat ea quis anim. Velit culpa duis quis qui excepteur excepteur nostrud.'
+        );
+    };
+
+    const changecaixaFormacaoText = () => {
+        setCaixaFormacaoText(
+            'Pariatur sunt pariatur ut nostrud dolor consectetur et id. Aliqua irure consectetur eu velit fugiat ea quis anim. Velit culpa duis quis qui excepteur excepteur nostrud.'
+        );
+    };
+    const changecaixaCompetenciasText = () => {
+        setcaixaCompetenciasText(
+            'Pariatur sunt pariatur ut nostrud dolor consectetur et id. Aliqua irure consectetur eu velit fugiat ea quis anim. Velit culpa duis quis qui excepteur excepteur nostrud.'
+        );
+    };
+    const changecaixaProjetosText = () => {
+        setcaixaProjetosText(
+            'Pariatur sunt pariatur ut nostrud dolor consectetur et id. Aliqua irure consectetur eu velit fugiat ea quis anim. Velit culpa duis quis qui excepteur excepteur nostrud.'
+        );
+    };
 
     return (
         <main>
             <section>
                 <div className={classes.homePageGrid}>
-
-
                     <div className={classes.colunaRedesSociais}>
-
                         <div className={classes.iconWrapper}>
-                            <img src={facebook} alt="facebook-logo" />
-                            <img src={linkedin} alt="linkedin-logo" />
-                            <img src={instagram} alt="linkedin-logo" />
+                            <Phone fontSize='large' />
+
+                            <Link href='https://linkedin.com/in/amanda-affanio-6a46571b7' target='_blank'>
+                                <LinkedIn fontSize='large' />
+                            </Link>
+                            <Link href='https://github.com/Rarymi ' target='_blank'>
+                                <GitHub fontSize='large' />
+                            </Link>
+                            <Link href='https://www.facebook.com/amanda.rarymi.5' target='_blank'>
+                                <Facebook fontSize='large' />
+                            </Link>
+                            <Link href='https://www.instagram.com/amandararymi/' target='_blank'>
+                                <Instagram fontSize='large' />
+                            </Link>
                         </div>
                     </div>
 
@@ -34,51 +63,48 @@ export default function HomePage() {
                                 <span>Olá, eu sou</span>
                                 <p>Amanda!</p>
                             </div>
-
                         </div>
                         <div className={classes.caixaEstudante}>
                             <div>
                                 <span>Estudante de</span>
                                 <p>Análise e Desenvolvimento de Sistemas</p>
                             </div>
-
                         </div>
-
                     </div>
                     <div className={classes.colunaInformacao}>
-
-                        <div className={classes.experienciaProfissional}
+                        <div
+                            className={classes.experienciaProfissional}
                             onMouseOver={() => changeExperienciaProfissionalText()}
-                            onMouseLeave={() => setExperienciaProfissionalText('Experiência Profissional')}>
-                            <div>
-                                {experienciaProfissionalText}
-                            </div>
-
-                        </div>
-                        <div className={classes.caixaFormacao}>
-                            <div>
-                                Formação Acadêmica
-                            </div>
-                        </div>
-                        <div className={classes.caixaCompetencias}>
-                            <div>
-                                Competências
-                            </div>
-                        </div>
-                        <div className={classes.caixaProjetos}>
-                            <div>
-                                Projetos
-                            </div>
+                            onMouseLeave={() => setExperienciaProfissionalText('Experiência Profissional')}
+                        >
+                            <div>{experienciaProfissionalText}</div>
                         </div>
 
+                        <div
+                            className={classes.caixaFormacao}
+                            onMouseOver={() => changecaixaFormacaoText()}
+                            onMouseLeave={() => setCaixaFormacaoText('Formação')}
+                        >
+                            <div>{caixaFormacaoText}</div>
+                        </div>
 
-
+                        <div
+                            className={classes.caixaCompetencias}
+                            onMouseOver={() => changecaixaCompetenciasText()}
+                            onMouseLeave={() => setcaixaCompetenciasText('Habilidades')}
+                        >
+                            <div>{caixaCompetenciasText}</div>
+                        </div>
+                        <div
+                            className={classes.caixaProjetos}
+                            onMouseOver={() => changecaixaProjetosText()}
+                            onMouseLeave={() => setcaixaProjetosText('Projetos')}
+                        >
+                            <div>{caixaProjetosText}</div>
+                        </div>
                     </div>
                 </div>
-
-
             </section>
         </main>
-    )
-
+    );
 }
